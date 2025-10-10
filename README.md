393f0e16f8a178b0f544dd3631f16e3aa881ea1d
Edward Leon <edleon761@gmail.com>
2025-10-10 14:04:41 -0700

    Documentation + new driver file
    - Updated the initial README for set up
    - Moved a lot of turbecs work to a separate gamestate file for easy installation

--- README.md
+++ README.md
@@ -16,8 +16,9 @@ When it comes to the extra data, they tend to fall into their own groups. This d
 #[turbo::game]
 struct GameState {
     
-    pub scene_data : SceneData,
+    pub scene_manager : SceneManager,
     pub entity_manager : EntityManager,
+    pub component_manager : ComponentManager,
     pub render_manager : Vec<Vec<usize>>,
 
     ...
@@ -93,8 +94,151 @@ And to get a sense of functions getting called every frame...
 4. on_???(&self (Entity) , _state : &mut GameState)
     - loops through all attached components
     - if the component getting looked at has something within this current lifetime cycle, then the code will move forward
-5. on_???(&self (Component) , _state : &mut GameState)
-    - Looks at the associated match case for the component type to see how to best move forward
+5. on_???(&self (Component) , ent : &mut Entity , state : &mut GameState)
+    - Looks at the associated match case for the component type to see how to move forward
+
+## How to make a project?
+
+In order to make a project with TurbECS, there are two general paths you can take.
+
+### Making a new project with TurbeECS (~5 minutes)
+
+#### Completion time : ~5 minutes
+
+Of the two paths, this is pretty easy!
+
+All you need to do is download the main turbecs folder [(this is the folder for reference)](./turbecs/), drop the folder in a preferred directory, and start adding your own work from there!
+
+- Note: it is heavily reccomended that you rename the two folders titled turbecs to the name of your project!
+
+### Implementing TurbECS into an existing Turbo Project 
+
+#### Completion time : ~15 minutes
+
+This'll take some work, but it shouldn't be too bad!
+
+#### 1. Download folders + file!
+
+Within the initial folder named the turbecs, download these folders + file:
+
+- [folder: assets](./turbecs/src/assets/)
+- [folder: turbecs](./turbecs/src/turbecs/)
+- [file: ecs_gamestate.rs](./turbecs/src/ecs_gamestate.rs)
+
+#### 2. Drop all the files into your src folder
+
+Here's where Turbo expects all the code within a project to be placed, so make that the files are placed here!
+
+#### 3. Add all necessary imports
+
+In your lib.rs file, add this block at the top, to link the rest of the files to the lib file
+
+```
+// Links the driver file to the main lib file
+mod ecs_gamestate;
+
+// Initial TurbECS import
+mod turbecs;
+
+// Manager imports
+use turbecs::{managers};
+
+use managers::entity_manager::EntityManager;
+use managers::component_manager::ComponentManager;
+use managers::scene_manager::SceneManager;
+
+// Community work/manager imports
+use turbecs::{particles::ParticleManager};
+```
+
+#### 4. Update your GameState to hold TurbECS
+
+In your GameState struct, add all necessary managers and optional managers that you'd like!
+
+```
+#[turbo::game]
+struct GameState {
+    
+    // Core managers for turbecs
+
+    pub scene_manager : SceneManager,
+    pub entity_manager : EntityManager,
+    pub component_manager : ComponentManager,
+    pub render_manager : Vec<Vec<usize>>,
+
+    // Community integrated work/managers
+
+    pub particle_manager : ParticleManager,
+
+    ...
+
+}
+```
+
+#### 5. Update your new() to initialize TurbECS
+
+Add any initializers for TurbECS managers to your new()!
+
+```
+impl GameState {
+    fn new() -> Self {
+
+        ...
+
+        Self {
+            
+            // Core managers for turbecs
+
+            scene_manager : SceneManager::new(),
+            entity_manager : EntityManager::new(),
+            component_manager : ComponentManager::new(),
+            render_manager : Vec::with_capacity(10),
+            
+            // Community integrated work/managers
+
+            particle_manager : ParticleManager::new(),
+
+            ...
+
+        }
+    
+    }
+
+    ...
+
+}
+```
+
+
+#### 6. Update your update() to hold TurbECS
+
+Since all the driver code for TurbECS, this is all you'll need to add to the update() functions are these two functions!
+
+```
+impl GameState {
+
+    ...
+
+    fn update(&mut self) {
+        
+        // Checks the scene state before continuing
+
+        self.check_scene_state();
+
+        // From here TurbECS will run it's lifetime functions!
+
+        self.run_lifetime();
+        
+        ...
+
+    }
+
+}
+```
+
+#### 7. Run the project!
+
+Once all is said and done, run the project as you have before, you should be good to go!
 
 ---
 ### For extra info, check the docs!
@@ -111,4 +255,4 @@ And to get a sense of functions getting called every frame...
 
 --- 
 
-Last updated : 10/4/2025
+Last updated : 10/10/2025
--- turbecs/src/lib.rs
+++ turbecs/src/lib.rs
@@ -1,38 +1,44 @@
+
+// Core Turbo functionality
 use turbo::*;
 
-// Initial imports
+// Links the driver file to the main lib file
+mod ecs_gamestate;
 
-use std::{collections::VecDeque};
-
+// Initial TurbECS import
 mod turbecs;
-use turbecs::{entity::Entity, managers};
+
+// Manager imports
+use turbecs::{managers};
+
+use managers::entity_manager::EntityManager;
+use managers::component_manager::ComponentManager;
+use managers::scene_manager::SceneManager;
+
+// Community work/manager imports
 use turbecs::{particles::ParticleManager};
 
-use managers::{entity_manager::EntityManager, component_manager::ComponentManager, scene_manager};
-
-use scene_manager::{SceneManager, Scenes};
+// Game specific elements that need to be in the GameState
 
 mod assets;
-
-use crate::turbecs::component_system;
-use component_system::{component::Component, component_types::ComponentTypes};
-use crate::turbecs::helpers::active_states::ActiveStates;
-
 use assets::game_state::{run_data::RunData};
+
 
 #[turbo::game]
 struct GameState {
     
+    // Core managers for turbecs
+
     pub scene_manager : SceneManager,
     pub entity_manager : EntityManager,
     pub component_manager : ComponentManager,
     pub render_manager : Vec<Vec<usize>>,
 
-    // Additional libraries/manager
+    // Community integrated work/managers
 
     pub particle_manager : ParticleManager,
 
-    // specfic game stuff
+    // Project specific elements
 
     pub run_data : RunData,
     pub can_interact : bool
@@ -44,213 +50,43 @@ impl GameState {
 
         camera::set_xy(0, 0);
 
-        Self {scene_manager : SceneManager::new(),
+        Self {
+            
+            // Core managers for turbecs
+
             entity_manager : EntityManager::new(),
             component_manager : ComponentManager::new(),
             render_manager : Vec::with_capacity(10),
             particle_manager : ParticleManager::new(),
             run_data : RunData::new(), can_interact : true}
     
-    }
 
     fn update(&mut self) {
         // Update the game & draw stuff
 
         self.check_scene_state();
+            
+            // Community integrated work/managers
+
+            particle_manager : ParticleManager::new(),
+
+            // Project specific elements
+
+            run_data : RunData::new(),
+            can_interact : true
+        }
+    
+    }
+
+    fn update(&mut self) {
+
+        // Checks the scene state before continuing
+
+        self.check_scene_state();
+
+        // From here TurbECS will run it's lifetime functions!
 
         self.run_lifetime();
 
     }
-}
-
-/*
-
-    Scene Systems!!!
-
-*/
-
-impl GameState {
-    
-    fn check_scene_state(&mut self) {
-
-        if self.scene_manager.is_loaded {
-
-            return;
-
-        }
-
-        for i in 0..self.entity_manager.entities.len() {
-            if (self.entity_manager.entities[i].state != ActiveStates::Destroyed) {
-                self.entity_manager.lifetime_data.new_destroy.push_back(i);
-            }
-        }
-
-        // Sanity check
-
-        log!("number of entities: {:?}", self.entity_manager.entities.len());
-        log!("number of entities to be destroyed : {:?}", self.entity_manager.lifetime_data.new_destroy.len());
-
-        self.on_destroy();
-
-        let mut new_ent = scene_manager::make_scene(self.scene_manager.active_scene);
-
-        
-        // Sanity check
-        log!("number of new entities : {:?}", new_ent.len());
-
-        self.new_entities(&mut new_ent);
-
-        self.scene_manager.is_loaded = true;
-
-        let len = borsh::to_vec(self).unwrap().len();
-        log!("LEN = {len}");
-
-    } 
-
-    pub fn new_entities(&mut self, _entities : &mut VecDeque<(Entity, VecDeque<Component>)>) {
-
-        while !_entities.is_empty()
-        {
-
-            let mut some_ent = _entities.front().unwrap().clone();
-            _entities.pop_front();
-
-            while !some_ent.1.is_empty() {
-
-                some_ent.0.comp_locats.push(self.component_manager.next_comp_locat().1);
-
-                self.component_manager.new_component(some_ent.1.front().unwrap().clone());
-                some_ent.1.pop_front();
-
-            }
-
-            for c in &some_ent.0.comp_locats {
-                self.component_manager.components[*c].init_has_x();
-            }
-
-            some_ent.0.init_has_x(self);
-
-            self.new_entity(&mut some_ent.0);
-
-        }
-        
-    }
-
-    pub fn new_entity(&mut self, _entity : &mut Entity) {
-
-        self.entity_manager.new_entities.push(_entity.clone());
-
-    }
-
-    fn get_num_of_free_locat(&mut self) -> usize {
-        return self.entity_manager.gap_data.empty_spaces.len();
-    }
-
-    fn get_next_free(&mut self) -> usize {
-
-        let next = *self.entity_manager.gap_data.empty_spaces.front().unwrap();
-
-        self.entity_manager.gap_data.empty_spaces.pop_front();
-
-        return next;
-
-    }
-
-}
-
-/*
-
-    Lifetime System!!!
-
-*/
-
-impl GameState {
-
-    fn run_lifetime(&mut self) {
-
-        self.load_entities();
-
-        self.on_awake();
-
-        self.on_start();
-
-        self.on_update();
-
-        self.on_destroy();
-
-        self.on_render();
-
-    }
-
-    pub fn load_entities(&mut self) {
-
-        if self.entity_manager.new_entities.len() == 0{
-            return;
-        }
-
-        for ent in self.entity_manager.new_entities.clone() {
-            self.load_entity(&mut ent.clone());
-        } 
-
-        self.entity_manager.new_entities.clear();
-
-    }
-
-    pub fn load_entity(&mut self, _entity : &mut Entity) {
-        let mut next : usize = 0;
-
-        if self.get_num_of_free_locat() > 0 {
-
-            next = self.get_next_free();
-            _entity.locat = next.clone();
-
-            self.entity_manager.entities[next] = _entity.clone();
-
-            // do linkage here!
-
-        }
-        else {
-
-            next = self.entity_manager.entities.len();
-            _entity.locat = next.clone();
-
-            self.entity_manager.entities.push(_entity.clone());
-
-            // do linkage here!
-
-        }
-
-        self.entity_manager.lifetime_data.new_awake.push_back(next);
-    }
-
-}
-
-/*
-* Helper functions 
-*/
-
-// Misc
-
-impl GameState {
-    
-    pub fn find_w_component(&mut self, some_type : ComponentTypes) -> (bool, usize) {
-
-        for i in 0..self.entity_manager.entities.len() {
-
-            if !self.entity_manager.entities[i].is_destroyed() {
-
-                let ent =  self.entity_manager.entities[i].clone();
-
-                if ent.has_component(some_type.clone(), self) {
-                    return (true, i);
-                }
-
-            }
-
-        }
-
-        return (false, 0);
-
-    }
-
 }
